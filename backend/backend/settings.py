f16db36f29adee65580105c09990d44ea1293b08
Joshua Levi <joshua.levi63@gmail.com>
2023-07-20 16:00:59 +0300

    restAPI

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend_app.apps.BackendAppConfig'
+    'rest_framework',
+    'base.apps.BackendAppConfig',
 ]
 
 MIDDLEWARE = [
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .products import products
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+
+    return Response('Hello')
+
+def getProducts(request): 
+
+    return JsonResponse(products, safe=False)
