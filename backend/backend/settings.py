af2c15748c9d494f21e5134033b886ee2d4e29ac
Joshua Levi <joshua.levi63@gmail.com>
2023-08-03 16:52:35 +0300

    reducerd

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_ROOT = '/images/'
+MEDIA_URL = '/images/'
 CORS_ALLOW_ALL_ORIGINS = True
 
 STATICFILES_DIRS = [
--- backend/base/views.py
+++ backend/base/views.py
@@ -2,26 +2,39 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Prodouct
 from .products import products
+from .serializer import ProdouctSerializer
+
 
 
 # Create your views here.
 
 @api_view(['GET'])
 def getRoutes(request):
-    return Response('Hello')
+    routes = [
+        '/api/products',
+        '/api/products/create',
+        '/api/products/upload',
+        '/api/products/<id>/reviews',
+        '/api/products/top',
+        '/api/products/<id>',
+    ]
+
+    return Response(products)
 
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Prodouct.objects.all()
+    serializer = ProdouctSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
+    product = Prodouct.objects.get(_id=pk)
+    serializer = ProdouctSerializer(product, many=False)
+    return Response(serializer.data)
 
-    return Response(product)
+
