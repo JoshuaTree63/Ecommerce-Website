571fb194df98f5437d9668e6a3b860f01543782d
Joshua Levi <joshua.levi63@gmail.com>
2023-09-16 22:43:31 +0300

    section 8

--- backend/base/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from django.contrib.auth.models import User
-from .models import Prodouct
-
-
-class ProdouctSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Prodouct
-        fields = '__all__'
---
+++ backend/base/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import Prodouct
+
+
+class UserSerializer(serializers.ModelSerializer):
+    first_name = serializers.SerializerMethodField(read_only=True)
+    last_name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', '_id', 'isAdmin']
+
+    def get__id(self, obj):
+        return obj.id
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+    def get_first_name(self, obj):
+        first_name = obj.first_name
+        if first_name == '':
+            first_name = obj.email
+        return first_name
+
+    def get_last_name(self, obj):
+        last_name = obj.last_name
+        if last_name == '':
+            last_name = obj.email
+        return last_name
+
+
+class UserSerializerwithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', '_id', 'isAdmin', 'token']
+
+    def get_token(elf, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
+
+
+class ProdouctSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Prodouct
+        fields = '__all__'
